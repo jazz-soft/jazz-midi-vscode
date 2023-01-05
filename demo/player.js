53ecfa2c7065afe99c5cd85b3eb1a914869d03a1
Sema <jazz-dev@jazz-soft.net>
2023-01-04 23:50:17 -0500

    Webview

--- demo/player.js
+++ demo/player.js
@@ -1,21 +1,39 @@
 const vscode = require('vscode');
 
-var provider = {
-    openCustomDocument: openCustomDocument,
-    resolveCustomEditor: resolveCustomEditor
-};
-
 function openCustomDocument(uri, context, token) {
     console.log('running openCustomDocument()');
+    return {
+        uri: uri,
+        dispose: function() {}
+    };
 }
 
 function resolveCustomEditor(document, panel, token) {
     console.log('running resolveCustomEditor()');
+    panel.webview.options = { enableScripts: true };
+    panel.webview.html = `<!DOCTYPE html>
+<html>
+<head>
+<script src="https://cdn.jsdelivr.net/npm/jzz"></script>
+<script src="https://cdn.jsdelivr.net/npm/jzz-midi-smf"></script>
+<script src="https://cdn.jsdelivr.net/npm/jzz-gui-player"></script>
+<script src="https://cdn.jsdelivr.net/npm/jzz-synth-tiny"></script>
+</head>
+<body>
+<div id="player"></div>
+<script>
+JZZ.synth.Tiny.register('Web Audio');
+var player = new JZZ.gui.Player('player');
+</script>
+</body>
+</html>`;
 }
 
 function activate(context) {
-    console.log('Activating extension!');
-    vscode.window.registerCustomEditorProvider('midi-demo.player', provider);
+    context.subscriptions.push(vscode.window.registerCustomEditorProvider('midi-demo.player', {
+        openCustomDocument: openCustomDocument,
+        resolveCustomEditor: resolveCustomEditor
+    }));
 }
 
 function deactivate() {}
