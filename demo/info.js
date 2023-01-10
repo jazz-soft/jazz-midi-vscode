f1d872e7321c42318d27e31ac276490fc1973ae6
Sema <jazz-dev@jazz-soft.net>
2023-01-10 00:42:34 -0500

    Updated WebView

--- demo/info.js
+++ demo/info.js
@@ -1,8 +1,32 @@
 const vscode = require('vscode');
 
+
 function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('midi-demo.info', function () {
-        vscode.window.showInformationMessage('Hello from midi-demo!');
+        const panel = vscode.window.createWebviewPanel(
+            'helloMidiPlayer', 'MIDI Info',
+            vscode.ViewColumn.One,
+            {
+                enableScripts: true
+            }
+        );
+        panel.webview.html =`<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h2>Web View</h2>
+<div id="wv"></div>
+<script>
+var ac = !!window.AudioContext;
+var wm = !!navigator.requestMIDIAccess;
+
+var txt = '<p>Web Audio:' + (ac ? '' : ' not') + ' supported</p>';
+txt += '<p>Web MIDI:' + (wm ? '' : ' not') + ' supported</p>';
+document.getElementById('wv').innerHTML = txt;
+</script>
+</body>
+</html>`;
     }));
 }
 
