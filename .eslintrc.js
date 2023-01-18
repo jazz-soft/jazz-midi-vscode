9952616af968eeb70e69b798416e9ec98b9859b3
Sema <jazz-dev@jazz-soft.net>
2023-01-17 22:48:32 -0500

    Added lint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended", 
+  "parserOptions": {
+    "ecmaVersion": 5
+  },
+  "overrides": [
+    {
+      "files": ["test/*"],
+      "parserOptions": {
+        "ecmaVersion": 2017
+      },
+      "globals": {
+        "describe": "readonly",
+        "it": "readonly"
+      }
+    },
+    {
+      "files": ["main.js"],
+      "globals": {
+        "JZZ": "readonly",
+        "define": "readonly",
+        "webkitAudioContext": "readonly"
+      },
+      "rules": {
+        "no-console" : "off",
+        "no-constant-condition" : ["error", { "checkLoops": false }],
+        "no-empty" : ["warn", { "allowEmptyCatch": true }],
+        "no-prototype-builtins" : "off"
+      }
+    }
+  ]
+};
