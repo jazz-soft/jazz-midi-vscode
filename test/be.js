5af3e7a51e4febd6bb94abfe4a231abc4cac00f5
Sema <jazz-dev@jazz-soft.net>
2023-01-22 00:07:02 -0500

    Backend coverage

--- test/be.js
+++ test/be.js
@@ -9,13 +9,29 @@ describe('backend', function() {
   });
   it('initView', function() {
     JMVSC.initView(vw);
+    vw.sendMessage({ type: 'jazz-midi' });
+    vw.sendMessage({ type: 'other' });
+  });
+  it('refresh', function() {
+    vw.sendMessage({ type: 'jazz-midi', detail: ['refresh'] });
+  });
+  it('other', function() {
+    vw.sendMessage({ type: 'jazz-midi', detail: ['other'] });
   });
 });
 
 function VW() {}
 function backend() {
-  VW.prototype.onDidReceiveMessage = function() {
+  VW.prototype.onDidReceiveMessage = function(f) {
+    this.hndl = f;
   }
+  VW.prototype.sendMessage = function(msg) {
+    if (this.hndl) this.hndl(msg);
+  }
+  VW.prototype.postMessage = function() {}
   vw = new VW();
   global.document = undefined;
+  const JZZ = require('jzz');
+  JZZ.addMidiIn('Fake MIDI-In', JZZ.Widget());
+  JZZ.addMidiOut('Fake MIDI-Out', JZZ.Widget());
 }
