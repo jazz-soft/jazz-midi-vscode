b9cda4220ba456b16481a9ed521555de6b849ec3
Sema <jazz-dev@jazz-soft.net>
2023-01-26 18:35:40 -0500

    Added screenshot

--- demo/main.js
+++ demo/main.js
@@ -1,9 +1,13 @@
+const JZZ = require('jzz');
 const player = require('./player');
 const info = require('./info');
 const note = require('./note');
 const scale = require('./scale');
 
 function activate(context) {
+    JZZ.addMidiIn('Virtual MIDI-In', JZZ.Widget());
+    JZZ.addMidiOut('Virtual MIDI-Out', JZZ.Widget());
+
     player.activate(context);
     info.activate(context);
     note.activate(context);
