7b329f8ad17c2064b20703482fe11b37f518b8d7
Sema <jazz-dev@jazz-soft.net>
2023-02-01 00:02:04 -0500

    Coverage

--- test/be.js
+++ test/be.js
@@ -1,58 +1,59 @@
 backend(); // VSCode Back End environment
 const assert = require('assert');
 const JMVSC = require('..');
-var vw, pan, fake_in;
+var vw1, vw2, pan2, pan1, fake_in;
 
 describe('backend', function() {
   it('context: backend', function() {
     assert.equal(JMVSC.context(), 'backend');
   });
   it('init', function() {
-    JMVSC.init(pan);
-    vw.sendMessage({ type: 'jazz-midi' });
-    vw.sendMessage({ type: 'other' });
+    JMVSC.init(pan1);
+    JMVSC.init(pan2);
+    vw1.sendMessage({ type: 'jazz-midi' });
+    vw1.sendMessage({ type: 'other' });
   });
   it('new', function() {
-    vw.sendMessage({ type: 'jazz-midi', detail: ['new'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['new'] });
   });
   it('refresh', function() {
-    vw.sendMessage({ type: 'jazz-midi', detail: ['refresh'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['refresh'] });
   });
   it('openout', function() {
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openout', 0, 'Not existent'] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openout', 0, 'Fake MIDI-Out 1'] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openout', 0, 'Fake MIDI-Out 1'] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openout', 0, 'Fake MIDI-Out 2'] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openout', 1, 'Fake MIDI-Out 1'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openout', 0, 'Not existent'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openout', 0, 'Fake MIDI-Out 1'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openout', 0, 'Fake MIDI-Out 1'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openout', 0, 'Fake MIDI-Out 2'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openout', 1, 'Fake MIDI-Out 1'] });
   });
   it('openin', function() {
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openin', 0, 'Not existent'] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openin', 0, 'Fake MIDI-In 1'] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openin', 0, 'Fake MIDI-In 1'] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openin', 0, 'Fake MIDI-In 2'] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['openin', 1, 'Fake MIDI-In 1'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openin', 0, 'Not existent'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openin', 0, 'Fake MIDI-In 1'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openin', 0, 'Fake MIDI-In 1'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openin', 0, 'Fake MIDI-In 2'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['openin', 1, 'Fake MIDI-In 1'] });
   });
   it('closeout', function() {
-    vw.sendMessage({ type: 'jazz-midi', detail: ['closeout', 0] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['closeout', 0] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['closeout', 0] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['closeout', 0] });
   });
   it('closein', function() {
-    vw.sendMessage({ type: 'jazz-midi', detail: ['closein', 0] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['closein', 0] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['closein', 0] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['closein', 0] });
   });
   it('midi', function() {
     fake_in._emit([]);
     fake_in._emit([0x90, 0x60, 0x7f]);
   });
   it('play', function() {
-    vw.sendMessage({ type: 'jazz-midi', detail: ['play', 0, 0x90, 0x60, 0x7f] });
-    vw.sendMessage({ type: 'jazz-midi', detail: ['play', 1, 0x90, 0x60, 0x7f] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['play', 0, 0x90, 0x60, 0x7f] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['play', 1, 0x90, 0x60, 0x7f] });
   });
   it('other', function() {
-    vw.sendMessage({ type: 'jazz-midi', detail: ['other'] });
+    vw1.sendMessage({ type: 'jazz-midi', detail: ['other'] });
   });
   it('exit', function() {
-    pan.dispose();
+    pan1.dispose();
   });
 });
 
@@ -72,8 +73,10 @@ function backend() {
   PAN.prototype.dispose = function() {
     if (this.onDis) this.onDis();
   }
-  vw = new VW();
-  pan = new PAN(vw);
+  vw1 = new VW();
+  vw2 = new VW();
+  pan1 = new PAN(vw1);
+  pan2 = new PAN(vw2);
   global.document = undefined;
   const JZZ = require('jzz');
   fake_in = JZZ.Widget();
